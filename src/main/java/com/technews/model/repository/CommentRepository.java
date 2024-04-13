46ff473b7078b7bd34775f2b5bbcce9a6ce3897e
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2024-04-13 03:48:43 -0400

    Created a repository package containing the interfaces to be implemented by the model classes in order to make the SQL queries through the JPA.

---
+++ src/main/java/com/technews/model/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.technews.model.repository;
+
+import com.technews.model.Comment;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository {
+    List<Comment> findAllCommentsByPostId(int postId);
+}
